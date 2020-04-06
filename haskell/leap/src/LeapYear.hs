7e1cad8bad92a1d5643081d336a711297b716b29
Edney Pitta <edney.pitta@farfetch.com>
2020-04-06 21:21:54 +0100

    Refactor haskel/leap to avoid unnecessary computations

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -5,8 +5,8 @@ divisibleBy x y = x `rem` y == 0
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | year `divisibleBy` 100 && year `divisibleBy` 400 = True
+  | not $ year `divisibleBy` 4 = False
+  | year `divisibleBy` 400 = True
   | year `divisibleBy` 100 = False
-  | year `divisibleBy` 4 = True
-  | otherwise = False
+  | otherwise = True
                    
