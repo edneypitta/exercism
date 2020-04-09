bb0dbf5c057de520ed066813d9f77b7bb001386c
Edney Pitta <edney.pitta@farfetch.com>
2020-04-09 23:27:09 +0100

    Build alphabet using ranges in haskell/pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -4,7 +4,4 @@ import Data.Char
 
 isPangram :: String -> Bool
 isPangram text = let loweredText = map toLower text
-                 in all (`elem` loweredText) alphabet 
-
-alphabet :: String
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+                 in all (`elem` loweredText) ['a'..'z'] 
