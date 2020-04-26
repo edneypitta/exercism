462e5448fb8381ed30089045445d2c74c924de0e
Edney Pitta <edney.pitta@farfetch.com>
2020-04-26 13:06:40 +0100

    Use Either monadic binding on haskell/nucleotide-count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -6,16 +6,14 @@ import qualified Data.Map as M
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = 
-    case mns of Nothing -> Left "invalid nucleotides"
-                Just ns -> Right $ M.fromListWith (+) $ empty ++ counts ns
-    where mns       = traverse toNucleotide xs
-          empty     = [(A, 0), (C, 0), (G, 0), (T, 0)]
-          counts ns = zip ns $ repeat 1
+nucleotideCounts xs = mns >>= return . toMap 
+    where mns   = traverse toNucleotide xs
+          toMap = foldr (M.adjust $ succ) empty 
+          empty = M.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 
-toNucleotide :: Char -> Maybe Nucleotide
-toNucleotide 'A' = Just A
-toNucleotide 'C' = Just C
-toNucleotide 'G' = Just G
-toNucleotide 'T' = Just T
-toNucleotide _   = Nothing
+toNucleotide :: Char -> Either String Nucleotide
+toNucleotide 'A' = Right A
+toNucleotide 'C' = Right C
+toNucleotide 'G' = Right G
+toNucleotide 'T' = Right T
+toNucleotide x   = Left $ "invalid nucleotide: " ++ show x
